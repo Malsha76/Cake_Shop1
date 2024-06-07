d3d22ac58304df6fa1ebf9b5f9735c48bd0dfbc0
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2024-06-07 13:39:14 +0530

    home

--- frontend/src/views/home.tsx
+++ frontend/src/views/home.tsx
@@ -8,11 +8,10 @@ render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | s
         <div className=" min-h-screen flex flex-col justify-center lg:flex-row lg:justify-between items-center lg:px-32 px-5 gap-10 bg-orange-100 font-bold text-amber-950 italic ">
           <div className=" w-full lg:w-2/4 space-y-4 mt-14 lg:mt-0">
             <h1 className="font-semibold text-5xl text-center lg:text-start leading-tight font-bold text-amber-950 italic">
-              Start your day with a steaming cup of coffee
+            Experience the sweetness at Sweet Bliss Cake Shop, where every cake is a masterpiece of flavor and artistry
             </h1>
             <p>
-              Boost your productivity and build your mood with a glass of coffee in
-              the morning
+            From classic favorites to custom-designed masterpieces, we cater to all your sweet desires. 
             </p>
 
             <div className=" flex flex-row gap-6">
@@ -24,9 +23,7 @@ render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | s
           <div className="relative">
             <img src="/src/assets/home.png" alt="img" />
 
-
-
-          </div>
+        </div>
         </div>
     );
   }
