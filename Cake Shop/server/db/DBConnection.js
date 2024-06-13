665b9b9d93fc2a895756279692f38b7d3cb88ac2
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2024-06-13 19:51:33 +0530

    server

---
+++ Cake Shop/server/db/DBConnection.js
@@ -0,0 +1,16 @@
+const mongoose =require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+const db= async () => {
+    try {
+        const coon = await mongoose.connect(MONGODB_URL);
+        console.info(`Mongo DB connect to : ${coon.connection.host}`);
+    } catch (error) {
+        console.error('Mongo DB Connection Error', error)
+    }
+}
+module.exports=db;
