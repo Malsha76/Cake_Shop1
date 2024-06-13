63543eebfb551ef3513d3dac2d41a4d97608cf0d
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2024-06-13 22:46:40 +0530

    server

--- Cake Shop/client/src/View/common/NavBar/NavBar.tsx
+++ Cake Shop/client/src/View/common/NavBar/NavBar.tsx
@@ -5,15 +5,15 @@ import logo from '../../../assets/Logo.png';
 export class NavBar extends Component {
     render() {
         return (
-            <div className="p-5 flex items-center justify-between bg-gradient-to-r from-green-400 to-blue-500 shadow-md rounded-lg">
+            <div className="p-5 flex items-center justify-between bg-gradient-to-r from-white-100 to-pink-200 shadow-md rounded-lg">
                 <Link to="/">
                     <div className="flex items-center">
-                        <img className="h-14 w-14 mr-3" src={logo} alt="Logo" />
+                        <img className="h-20 w-40 mr-3" src={logo} alt="Logo" />
                         <h1 className="text-2xl text-white font-bold">Cuppy Cake</h1>
                     </div>
                 </Link>
 
-                <ul className="flex space-x-8 font-semibold text-white">
+                <ul className="flex space-x-8 font-semibold text-black">
                     <li className="hover:text-gray-300">
                         <Link to="/">Home</Link>
                     </li>
@@ -32,7 +32,7 @@ export class NavBar extends Component {
                 </ul>
 
                 <button
-                    className="border-2 border-white font-semibold text-white bg-green-500 hover:bg-green-600 px-4 py-2 rounded-full transition duration-300">
+                    className="border-2 border-purple font-semibold text-black bg-purple-300 hover:bg-purple-400 px-4 py-2 rounded-full transition duration-300">
                     <Link to="/login">Sign in</Link>
                 </button>
             </div>
