22a162da6357f9e6019d44ee0ac124180e8482a1
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2024-06-13 17:07:46 +0530

    client

---
+++ Cake Shop/client/src/views/common/BackGround/BackGround.tsx
@@ -0,0 +1,11 @@
+export const BackGround = () => {
+    return (
+        <>
+            <div className="bg-cover bg-center h-screen" style={{ backgroundImage: 'url("client/src/assets/cake_background.jpg")' }}>
+                {}
+            </div>
+        </>
+    );
+};
+
+export default BackGround;
