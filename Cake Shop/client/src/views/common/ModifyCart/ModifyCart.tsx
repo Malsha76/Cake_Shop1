c286da697ef5d99ad6f58564429aed1c09884898
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2024-06-13 15:02:24 +0530

    client

--- Cake Shop/client/src/views/common/ModifyCart/ModifyCart.tsx
+++ Cake Shop/client/src/views/common/ModifyCart/ModifyCart.tsx
@@ -15,6 +15,7 @@ export class ModifyCart
             itemCount: 1
         }
     }
+
     componentDidMount() {
         const  {itemCount} = this.state;
 
@@ -33,36 +34,52 @@ export class ModifyCart
         }
         console.log(ModifyCart.itemsList);
     }
-}
-componentDidUpdate(prevProps: Readonly<ModifyCartProps>, prevState: Readonly<ModifyCartState>, snapshot?: any) {
-    let {itemCount} = this.state;
-    let item =  ModifyCart
-        .itemsList
-        .find(item =>
-            item.product.id ===
-            this.props.data.product.id);
-            let index = ModifyCart.itemsList.indexOf(item);
-            ModifyCart.itemsList.splice(index, 1);
-            ModifyCart.itemsList.push(
-                {product: this.props.data.product,
-                    itemCount: itemCount}
-            );
+
+    componentDidUpdate(prevProps: Readonly<ModifyCartProps>, prevState: Readonly<ModifyCartState>, snapshot?: any) {
+        let {itemCount} = this.state;
+        let item =  ModifyCart
+            .itemsList
+            .find(item =>
+                item.product.id ===
+                this.props.data.product.id);
       
-            console.log(ModifyCart.itemsList)
+        let index = ModifyCart.itemsList.indexOf(item);
+        ModifyCart.itemsList.splice(index, 1);
+        ModifyCart.itemsList.push(
+            {product: this.props.data.product,
+                itemCount: itemCount}
+        );
+
+        console.log(ModifyCart.itemsList)
+    }
+
+    render() {
+
+        let {itemCount} = this.state;
+
+        const onDecreaseItemCount = ()=> {
+            if (itemCount > 1) {
+                this.setState({itemCount: --itemCount})
+            } else {
+                alert("Item count can't be less than 1");
+            }
         }
-        render() {
+        const onIncreaseItemCount = () => {
+            this.setState({itemCount: ++itemCount})
+        }
 
-            let {itemCount} = this.state;
-    
-            const onDecreaseItemCount = ()=> {
-                if (itemCount > 1) {
-                    this.setState({itemCount: --itemCount})
-                } else {
-                    alert("Item count can't be less than 1");
-                }
-            }
-            const onIncreaseItemCount = () => {
-                this.setState({itemCount: ++itemCount})
-            }
-
-            
+        return (
+            <div className="w-full mt-1 p-[2.4px]
+                                            bg-pink-400 text-[20px]
+                                            text-center">
+                <button className="float-left text-[20px]
+                                        bg-pink-300 rounded-lg h-8 w-8"
+                                  onClick={onDecreaseItemCount}>-</button>
+                <small className="text-[20px]">{itemCount}</small>
+                <button className="float-right text-[20px]
+                                        bg-pink-300 rounded-lg h-8 w-8"
+                                  onClick={onIncreaseItemCount}>+</button>
+            </div>
+        );
+    }
+}
