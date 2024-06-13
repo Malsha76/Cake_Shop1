d6599ebb43aa9409a6b034dcdeb2cb494ecca036
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2024-06-13 17:01:15 +0530

    client

--- Cake Shop/client/src/views/common/Product/Product.tsx
+++ Cake Shop/client/src/views/common/Product/Product.tsx
@@ -22,5 +22,20 @@ export class Product extends Component<ProductProps, ProductState> {
         const {data}=this.props;
         console.log(data.image)
         const image = require('../../../assets/product/' + data.image)
-     
-}
+        
+        return (
+            <>
+              { <div className={'relative px-4 py-2 justify-center items-center  bg-white mr-2 mb-2 border-gray-800'}>
+                    <div>
+                        <img src={image} alt=""/>
+                    </div>
+
+                    <div className={'flex'}>
+
+                        <div>
+                            <h3 className={'text-red-700 text-[#000000] text-[18px] pl-2 pr-4 hover:bg-green-200 '}>{data.name}</h3>
+                        </div>  
+
+</>
+);
+              }
