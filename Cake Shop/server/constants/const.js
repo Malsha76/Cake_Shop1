53dd0d8fa7a4982be55ba9c66c9a990bce5da36b
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2024-06-13 19:49:28 +0530

    server

---
+++ Cake Shop/server/constants/const.js
@@ -0,0 +1,48 @@
+const productsList = [
+    {
+      "id": 1,
+      "name": "Choclate Cake",
+      "price": "2300",
+      "currency": "LKR",
+      "image": "menu1.jpg"
+    },
+    {
+      "id": 2,
+      "name": "pinnaple Gato",
+      "price": "1600",
+      "currency": "LKR",
+      "image": "menu2.jpg"
+    },
+    {
+      "id": 3,
+      "name": "Choclate Chips Gato",
+      "price": "2200",
+      "currency": "LKR",
+      "image": "menu3.jpg"
+    },
+    {
+      "id": 4,
+      "name": "Birthday Cake",
+      "price": "1850",
+      "currency": "LKR",
+      "image": "menu4.jpg"
+    },
+    {
+      "id": 5,
+      "name": "Aniversary Cake",
+      "price": "3000",
+      "currency": "LKR",
+      "image": "menu5.jpg"
+    },
+    {
+      "id": 6,
+      "name": "Butterfly Cake",
+      "price": "2000",
+      "currency": "LKR",
+      "image": "menu6.jpg"
+    }
+  ];
+
+  const test = [];
+
+module.exports = {productsList};
