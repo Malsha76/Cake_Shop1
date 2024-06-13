30e36f84383501c7fd697ea8d9306af7fb5f1ffd
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2024-06-13 15:19:05 +0530

    client

--- Cake Shop/client/src/views/Pages/home/Home.tsx
+++ Cake Shop/client/src/views/Pages/home/Home.tsx
@@ -16,54 +16,61 @@ export class Home extends Component {
         }
     }
 
+
+   
     componentDidMount() {
         this.fetchData();
-        fetchData= async ()=>{
-            try {
-                this.api.get('/products/all').then((res:{data:any}) =>{
-                    const jsonData=res.data;
-                    this.setState({data:jsonData});
-                }).catch((error:any)=>{
-                    console.log("Axios Error");
-                }) 
+            
 
-}catch (error){
-    console.log("Data Not Found")
-}
+    }
+
+    fetchData= async ()=>{
+        try {
+            this.api.get('/products/all').then((res:{data:any}) =>{
+                const jsonData=res.data;
+                this.setState({data:jsonData});
+            }).catch((error:any)=>{
+                console.log("Axios Error");
+            })
+
+        }catch (error){
+            console.log("Data Not Found")
         }
+    }
 
-        render() {
+     render() {
 
-            // @ts-ignore
-            const {data}=this.state;
-            return (
-                <>
-                    <div className='ricePlate flex justify-center items-center gap-44 bg-pink-200 from-fuchsia-200'>
-                        <div className={'pl-32'}>
-                            <h4 className='text-2xl font-bold'>WELCOME COME TO</h4>
-                            {/*<h1 className='text-8xl font-bold'>to</h1>*/}
-                            <h1 className='text-8xl font-bold'>Cuppy Cake</h1>
-                            {/*<h1 className='text-8xl font-bold'>for you</h1>*/}
-                            <h6 className={''}>where every slice is a piece of heaven!</h6>
-                            <button className='bg-red-600 rounded-full p-2 text-2xl mt-20' onClick={this.latestMenuList}>Latest Collection</button>
+       
+        const {data}=this.state;
+        return (
+            <>
+                <div className='ricePlate flex justify-center items-center gap-44 bg-pink-200 from-fuchsia-200'>
+                    <div className={'pl-32'}>
+                        <h4 className='text-2xl font-bold'> WELCOME TO</h4>
+                       <h1 className='text-8xl font-bold'>Cuppy Cake</h1>
+                        <h6 className={''}>where every slice is a piece of heaven!  </h6>
+                        <button className='bg-red-600 rounded-full p-2 text-2xl mt-20' onClick={this.latestMenuList}>Latest Collection</button>
                     </div>
                     <div className={''}>
-                        <img className='scale-95 ' src={ricePlate} alt='ricePlate'/>
-                    </div>
+                        <img className='scale-95 ' src={cake} alt='cake'/>
                     </div>
 
-<div className={'flex'}>
-    <div className={'flex flex-wrap justify-center items-center mx-auto mt-5 mb-5 shadow-md '}>
+                </div>
 
-        {
-           data.map((product:any)=>( //loop array
-               <Product key={product.id} data={product}/>
-           ) )
-        }
-    </div>
-    </div>
+                <div className={'flex'}>
+                    <div className={'flex flex-wrap justify-center items-center mx-auto mt-5 mb-5 shadow-md '}>
+
+                        {
+                           data.map((product:any)=>( 
+                               <Product key={product.id} data={product}/>
+                           ) )
+                        }
+                    </div>
+                </div>
             </>
         );
+
+
     }
 
     private latestMenuList = () => {
