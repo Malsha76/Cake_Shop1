95bbabe2c2b2b4ddbaa7bf16956acbd1e0f834e2
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2024-06-13 16:29:01 +0530

    client

---
+++ Cake Shop/client/src/views/Pages/banner/Banner.tsx
@@ -0,0 +1,25 @@
+import {Component} from "react";
+import ricePlate from "../../../assets/cake.jpg";
+
+export class Banner extends Component {
+    render() {
+        return (
+            <>
+                <div className='ricePlate flex justify-center items-center gap-44 bg-pink-200 from-fuchsia-200'>
+                    <div className={'pl-32'}>
+                        <h4 className='text-2xl font-bold'>WELCOME TO</h4>
+                       <h1 className='text-8xl font-bold'>Cuppy Cake</h1>
+                        
+                        <h6 className={''}> where every slice is a piece of heaven! </h6>
+                        <button className='bg-red-600 rounded-full p-2 text-2xl mt-20'>Latest Collection</button>
+                    </div>
+                    <div className={''}>
+                        <img className='scale-95 ' src={cake} alt='cake'/>
+                    </div>
+
+                </div>
+
+            </>
+        );
+    }
+}
