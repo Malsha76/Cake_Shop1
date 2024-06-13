d87972600df2e81ebf9dfceac52c5f7ef1b89c3e
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2024-06-13 15:05:26 +0530

    client

---
+++ Cake Shop/client/src/views/Pages/home/Home.tsx
@@ -0,0 +1,21 @@
+import {Component} from "react";
+import axios from "axios";
+import ricePlate from "../../../assets/cake.jpg";
+import {privateDecrypt} from "crypto";
+
+
+export class Home extends Component {
+    private api: any;
+
+
+    constructor(props: {}| Readonly <{}>) {
+        super(props);
+        this.api = axios.create({baseURL: `http://localhost:4000`})
+        this.state={
+            data:[],
+        }
+    }
+
+
+   
+}
