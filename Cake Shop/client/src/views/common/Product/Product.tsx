79278906c410104533b5bf9d60963ffa8b3f4858
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2024-06-13 16:58:01 +0530

    client

---
+++ Cake Shop/client/src/views/common/Product/Product.tsx
@@ -0,0 +1,12 @@
+import {Component} from "react";
+import {ModifyCart} from "../ModifyCart/ModifyCart";
+
+interface ProductProps {
+    data: any
+}
+
+interface ProductState {
+    isActive: boolean
+}
+
+
