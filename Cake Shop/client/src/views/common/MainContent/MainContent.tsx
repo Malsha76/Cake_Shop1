f3356d7701dc0f483e616ed2ca358877a646f9f4
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2024-06-13 16:53:39 +0530

    client

---
+++ Cake Shop/client/src/views/common/MainContent/MainContent.tsx
@@ -0,0 +1,32 @@
+import {Component} from "react";
+import {Route, Routes} from "react-router-dom";
+import {Home} from "../../pages/home/Home";
+import {About} from "../../pages/about/About";
+import {Contact} from "../../pages/contact/Contact";
+import {ShoppingCart} from "../../pages/ShoppingCart/ShoppingCart";
+import {ModifyCart} from "../ModifyCart/ModifyCart";
+import ricePlate from '../../../assets/cake.jpg'
+import {Product} from "../Product/Product";
+
+export class MainContent extends Component {
+    render() {
+        return (
+            <>
+            <div className={" mx-auto "}>
+               
+                <Routes>
+                    <Route path='/'  Component={Home}></Route>
+                    <Route path='/about' Component={About}></Route>
+                    <Route path="/shopping-cart"
+                           element={<ShoppingCart
+                               itemsList={ModifyCart.itemsList}/>
+                           }>
+                    </Route>
+                    <Route path='/contact' Component={Contact}></Route>
+
+                </Routes>
+            </div>
+            </>
+        );
+    }
+}
