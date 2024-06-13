0318ff2ecc1ffcced99f62dde5a1da43dd2bc7e8
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2024-06-13 14:59:48 +0530

    client

--- Cake Shop/client/src/views/common/ModifyCart/ModifyCart.tsx
+++ Cake Shop/client/src/views/common/ModifyCart/ModifyCart.tsx
@@ -34,6 +34,19 @@ export class ModifyCart
         console.log(ModifyCart.itemsList);
     }
 }
-    
+componentDidUpdate(prevProps: Readonly<ModifyCartProps>, prevState: Readonly<ModifyCartState>, snapshot?: any) {
+    let {itemCount} = this.state;
+    let item =  ModifyCart
+        .itemsList
+        .find(item =>
+            item.product.id ===
+            this.props.data.product.id);
+            let index = ModifyCart.itemsList.indexOf(item);
+            ModifyCart.itemsList.splice(index, 1);
+            ModifyCart.itemsList.push(
+                {product: this.props.data.product,
+                    itemCount: itemCount}
+            );
       
-  
+            console.log(ModifyCart.itemsList)
+        }
