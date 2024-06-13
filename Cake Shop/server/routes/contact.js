ad7a3ca3abc15c9ca8f8dd943e76bca4fa71573d
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2024-06-13 22:29:04 +0530

    server

--- Cake Shop/server/routes/contact.js
+++ Cake Shop/server/routes/contact.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const ContactController = require('../controller/contactController');
 const router = express.Router();
+const db= require('./db/DBConnection');
 
+db();
 router.post('/submit', ContactController.submitContactDetails);
 
 
